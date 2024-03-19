17b03d205d262cefe3e1c4f0b5997d2217985bf8
Eli Sutton <elisutton0721@gmail.com>
2024-03-19 15:33:49 -0400

    Fix script
    Only creates directories and downloads data if data file exists

--- module_5_batch_processing/code/download_data.sh
+++ module_5_batch_processing/code/download_data.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Get taxi type and year as command line args
-TAXI_TYPE=$1 # "yellow"
+TAXI_TYPE=$1 # yellow
 YEAR=$2 # 2020
 
 URL_PREFIX="https://github.com/DataTalksClub/nyc-tlc-data/releases/download"
@@ -13,16 +13,19 @@ MONTH=1
 while [ "${MONTH}" -le 12 ]; do
     # Format month as 01,02,...,11,12
     FMONTH=$(printf "%02d" "${MONTH}")
-
     URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
-
     LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
     LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
     LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
-
-    echo "downloading ${URL} to ${LOCAL_PATH}"
-    mkdir -p "../${LOCAL_PREFIX}"
-    # wget "${URL}" -O "${LOCAL_PATH}"
-    # curl -L "${URL}" -O "${LOCAL_PATH}"
+    HTTP_RESPONSE=$(curl --head --write-out '%{http_code}' --silent \
+        --output /dev/null \
+        -L "${URL}")
+    
+    # Only create directories and download data if data file is found
+    if [ "${HTTP_RESPONSE}" = "200" ]
+    then
+        echo "Downloading ${URL} to ${LOCAL_PREFIX}"
+        curl --create-dirs --output "${LOCAL_PATH}" -L "${URL}"
+    fi
     MONTH=$((MONTH + 1))
 done
